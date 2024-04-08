220f65cc38f6c3f7d3d5a403d350469832650ff0
sixthson6 <sudais2408@gmail.com>
2024-04-08 02:03:55 +0000

    deployment corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
--- setup web server for application deployment
-
-sudo apt-get update -y
-
-sudo apt-get install nginx -y
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-echo -e "this is an nginx server" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown --recursive ubuntu:ubuntu /data/
-
-my_setup="server {
-	listen 80;
-	listen [::]:80;
-
-	root /var/www/html;
-	server_name _;
-	
-	location / {
-		try_files $uri $uri/ =404;
-	}
-
-	location /hnbn_static/ {
-		alias /data/web_static/current/;
-	}
-	
-	error_page 404 /404.html;
-
-	location = /404.html {
-		internal;
-	}
-};"
-
-echo -e "$my_setup" | sudo tee /etc/nginx/sites-available/default > /dev/null
-=======
 # script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
