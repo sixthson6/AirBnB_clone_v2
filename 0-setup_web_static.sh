170c1f91808cb4868f4bacab6362654e111c4606
sixthson6 <sudais2408@gmail.com>
2024-04-08 02:27:25 +0000

    deployment corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,6 +23,5 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
->>>>>>> 81236c8907067292981ee63fe807a410088415fa
 
 sudo service nginx restart
