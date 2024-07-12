a5e0d02e1e9edb09933fb19a7582eace1a06aedf
sixthson6 <sudais2408@gmail.com>
2024-07-11 22:47:20 -0700

    deploy airbnb clone

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,16 @@
 
 
 import re
-from fabric.api import run, env
+from fabric.api import *
 import os
 
+
+env.hosts = [
+        '54.208.116.17',
+        '54.165.202.243'
+        ]
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
     """transfer archive to remote
@@ -16,28 +23,25 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
 
-    env.hosts = [
-            '54.208.116.17',
-            '416793-web-01',
-            '54.165.202.243',
-            '416793-web-02'
-            ]
-    env.user = 'ubuntu'
-    env.key_filename = '~/.ssh/id_rsa'
-
     # file = web_static_20240711195239.tgz
 
     pattern = r' _[0-9]+'
     match = re.findall(pattern, archive_path)
     name = "web_static" + match
 
-    with cd("/tmp"):
-        a = put(local_path="{}".format(archive_path), remote_path="/data/web_static/releases/{}".format(name))
-        unar = run("tar -xzvf /data/web_static/releases/{}".format(name))
-        if unar.succeded is True:
-            b = run("rm /data/web_static/releases/{}".format(name))
-    c = run("rm -f /data/web_static/current")
-    d = run("ln -s /data/web_static/releases/{} /data/web_static/current".format(name))
+    put(archive_path, "/tmp")
+    run("sudo mkdir -p /data/web_static/releases/{}".format(name))
+
+    unar = run("sudo tar -xzvf /tmp/{}.tgz -C /data/web_static/releases/web_static_{}/".format(name))
+    if unar.succeded is True:
+            b = run("sudo rm /tmp/{}".format(name))
+
+    run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+
+    run('sudo rm -rf /data/web_static/releases/{}/web_static'.format(name))
+
+    c = run("sudo rm -f /data/web_static/current")
+    d = run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(name))
 
     if a.ok and b.ok and c.ok and d.ok:
         return True
