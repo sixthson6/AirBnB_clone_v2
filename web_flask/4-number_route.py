11c398e0bb3a2dc4729bd3ae5c94b6874ceea9fc
sixthson6 <sudais2408@gmail.com>
2024-07-26 17:33:47 +0000

    n is a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python3
+"""
+Flask
+hello
+hbnb
+c is cool
+n is a number
+python is cool
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
@@ -8,11 +19,13 @@ def hello_hbnb():
     """
     return "Hello HBNB"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """return hbnb
     """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
@@ -21,11 +34,13 @@ def c_text(text):
     text_mod = text.replace('_', ' ')
     return "C {}".format(text_mod)
 
+
 @app.route("/python", strict_slashes=False)
 def python_def():
     """Return Python is cool
     """
     return "Python is cool"
+
 
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is_cool"):
@@ -34,12 +49,14 @@ def python_text(text="is_cool"):
     text_mod = text.replace('_', ' ')
     return "Python {}".format(text_mod)
 
-@app.route("/number/<n>", strict_slashes=False)    
+
+@app.route("/number/<n>", strict_slashes=False)
 def number_n(n):
     """Return n is a number
     """
     n = int(n)
     return "{} is a number".format(n)
 
+
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
