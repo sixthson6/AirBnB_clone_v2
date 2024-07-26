3d8b7001e71dc50cfccc4bdcc1e107bc3a87f737
sixthson6 <sudais2408@gmail.com>
2024-07-26 04:26:41 +0000

    python is ???

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    text_mod = text.replace('_', ' ')
+    return "C {}".format(text_mod)
+
+@app.route("/python", strict_slashes=False)
+def python_def():
+    return "Python is cool"
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is_cool"):
+    text_mod = text.replace('_', ' ')
+    return "Python {}".format(text_mod)
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
