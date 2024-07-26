049c05f77fb431c22b5899b4b88dbb32245a79a7
sixthson6 <sudais2408@gmail.com>
2024-07-26 17:29:00 +0000

    c is cool

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""
+Flask
+hello hbnb
+hbnb
+c is cool
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
@@ -8,11 +17,13 @@ def hello_hbnb():
     """
     return "Hello HBNB"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Returns hbnb
     """
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def replace_text(text):
@@ -21,5 +32,6 @@ def replace_text(text):
     text_mod = text.replace('_', ' ')
     return "C {}".format(text_mod)
 
+
 if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
